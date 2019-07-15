854fed830726549631fe92dc9967c03bd358343e
wolfkoning <scott.c.vh@gmail.com>
2019-07-14 20:41:36 -0600

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import moment from "moment";
+import axios from 'axios';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+
+import NavigationContainer from './navigation/navigation-container';
+import Home from './pages/home';
+import About from './pages/about';
+import Contact from './pages/contact';
+import Blog from './pages/blog';
+import PortfolioDetail from './portfolio/portfolio-detail';
+import NoMatch from "./pages/no-match";
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <div className='app'>
+        <Router>
+          <div>
+            <h1>Moment</h1>
+            <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
+
+            <NavigationContainer />
+            
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/blog" component={Blog} />
+              <Route exact path="/portfolio/:slug" component={PortfolioDetail} />
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+        </Router>
+
+      </div>
+    );
+  }
+}
---
+++ src/components/navigation/navigation-container.js
@@ -0,0 +1,22 @@
+
+import React, { Component } from "react";
+import { NavLink } from 'react-router-dom';
+
+export default class NavigationComponent extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <NavLink exact to="/">Home</NavLink>
+        <NavLink exact to="/about" activeClassName="nav-link-active">About</NavLink>
+        <NavLink exact to="/contact" activeClassName="nav-link-active">Contact</NavLink>
+        <NavLink exact to="/blog" activeClassName="nav-link-active">Blog</NavLink>
+        {false ? <button>Add Blog</button> : null}
+
+    </div>
+    );
+  }
+}
